ece4198ff702044591a7d42ab8797da17c0a7d65
nagaraju.gorumutchu <nagaraju.gorumutchu@servicenow.com>
2021-10-14 11:12:58 +0530

    2006:randfile_2006

--- deleteLater/multiCommits.sh
+++ deleteLater/multiCommits.sh
@@ -1,7 +1,7 @@
 string=""
 x=1
-j=1
-while [ $x -le 2000 ]
+j=2005
+while [ $x -le 4000 ]
 do
  string="touch randfile$((j+x)) && git add . && git commit -m '$((j+x)):randfile_$((j+x))'"
  echo $string
